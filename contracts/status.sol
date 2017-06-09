7777586713db2dcc6ae3c7a8e1c222c66db4d614
Siddesh Sangodkar <siddesh226@gmail.com>
2017-06-09 16:35:06 +0530

    updated interest_rate  calculation

--- contracts/status.sol
+++ contracts/status.sol
@@ -1,159 +1,6 @@
 
 pragma solidity ^0.4.2;
 
-
-
-contract RequestContract { // Request Contract
-
-
-    struct Request { // Request structure
-        uint reqId;
-        address from;
-        address to;
-        bytes32 status;
-        uint256 date;
-        uint256 amount;
-        uint256 duration;
-        bytes32 purpose;
-         }
-    uint totalReq = 0;
-    // map of Requests
-    mapping(uint => Request) request;
-    mapping(address => uint[]) incomingRequest;
-    mapping(address => uint[]) outgoingRequest;
-   
-
-    //Events for requests
-    event RequestMoney( uint indexed reqId,address indexed from, address indexed to);
-    event AcceptRequest(uint indexed reqId, address indexed from, address indexed to);
-   // event RejectRequest(uint indexed reqid, address indexed from, address indexed to);
-       event Payback(uint indexed reqid, address indexed from, address indexed to);
-
-    function borrowRequest(address userId,uint256 amount, uint256 duration, bytes32 purpose) {
-
-        totalReq++;
-        request[totalReq] = Request({
-            reqId: totalReq,
-            from:msg.sender,
-            to: userId,
-            status: "requested",
-            date: now,
-            amount: amount ,
-            duration: duration,
-            purpose: purpose
-          
-          
-        });
-
-        outgoingRequest[msg.sender].push(totalReq);
-        incomingRequest[userId].push(totalReq);
-        RequestMoney(totalReq,userId, msg.sender); // request event
-
-
-    }
-    
-    // accept request function
-    function acceptRequest(uint reqid) payable {
-        if (reqid > totalReq || reqid <= 0 || msg.value != request[reqid].amount || request[reqid].status !='requested' ) {
-            throw;
-        } else {
-            if (request[reqid].to == msg.sender) {
-                request[reqid].status = "accepted";
-                  request[reqid].from.send(msg.value);
-                
-                            }
-        }
-        AcceptRequest(reqid, msg.sender, request[reqid].from); // accept event
-    }
-    
-     // payback request function
-    function payback(uint reqid) payable {
-        if (reqid > totalReq || reqid <= 0 || msg.value != request[reqid].amount || request[reqid].status !='accepted'   ) {
-            throw;
-        } else {
-            if (request[reqid].from == msg.sender) {
-                request[reqid].status = "settled";
-                  request[reqid].to.send(msg.value);
-                
-                }
-        }
-        Payback(reqid, msg.sender, request[reqid].to); // accept event
-    }
-   
-    // reject request function
-    // function rejectRequest(uint reqid) {
-    //     if (reqid > totalReq && reqid <= 0) {
-    //         throw;
-    //     } else {
-    //         if (request[reqid].to == msg.sender) {
-    //             request[reqid].status = "rejected";
-    //                       }
-    //     }
-    //     RejectRequest(reqid, msg.sender, request[reqid].from); // reject event
-    // }
-    
-    // get incoming request function
-    function getIncomingRequests() constant returns(uint[]  memory, address[]  memory,  bytes32[]  memory, uint256[]  memory, uint256[]  memory, uint256[]  memory , bytes32[]  memory) {
-       
-       uint[] memory id;
-       address[]  memory from;
-       bytes32[] memory status;
-       uint256[]  memory date;
-       uint256[] memory  amount;
-       uint256[] memory duration;
-       bytes32[] memory purpose;
-       
-       id = new uint[](incomingRequest[msg.sender].length);
-       from = new address[](incomingRequest[msg.sender].length);
-       status = new bytes32[](incomingRequest[msg.sender].length);
-       date = new uint256[](incomingRequest[msg.sender].length);
-       amount = new uint256[](incomingRequest[msg.sender].length);
-       duration = new uint256[](incomingRequest[msg.sender].length);
-       purpose = new bytes32[](incomingRequest[msg.sender].length);
-        for(uint i=0; i< incomingRequest[msg.sender].length;i++) {
-            uint  reqId = incomingRequest[msg.sender][i];
-            id[i] = request[reqId].reqId;
-            from[i] = request[reqId].from;
-            status[i] = request[reqId].status;
-            date[i] = request[reqId].date; 
-            amount[i] = request[reqId].amount;
-            purpose[i] = request[reqId].purpose;
-         }
-         return (id,from,status,date,amount,duration,purpose);
-    }
-    
-     // get outgoing request function
-    function getOutgoingRequests() constant returns(uint[]  memory, address[]  memory,  bytes32[]  memory, uint256[]  memory, uint256[]  memory, uint256[]  memory , bytes32[]  memory) {
-       
-       uint[] memory id;
-       address[]  memory from;
-       bytes32[] memory status;
-       uint256[]  memory date;
-       uint256[] memory  amount;
-       uint256[] memory duration;
-       bytes32[] memory purpose;
-       
-       id = new uint[](outgoingRequest[msg.sender].length);
-       from = new address[](outgoingRequest[msg.sender].length);
-       status = new bytes32[](outgoingRequest[msg.sender].length);
-       date = new uint256[](outgoingRequest[msg.sender].length);
-       amount = new uint256[](outgoingRequest[msg.sender].length);
-       duration = new uint256[](outgoingRequest[msg.sender].length);
-       purpose = new bytes32[](outgoingRequest[msg.sender].length);
-        for(uint i=0; i< outgoingRequest[msg.sender].length;i++) {
-            uint  reqId = outgoingRequest[msg.sender][i];
-            id[i] = request[reqId].reqId;
-            from[i] = request[reqId].from;
-            status[i] = request[reqId].status;
-            date[i] = request[reqId].date; 
-            amount[i] = request[reqId].amount;
-            purpose[i] = request[reqId].purpose;
-         }
-         return (id,from,status,date,amount,duration,purpose);
-    }
-    
-
-}
 contract AccountContract { // Account contract 
     struct LenderAccount //  LenderAccount structure
     {
@@ -293,8 +140,171 @@ contract AccountContract { // Account contract
 
 }
 
+contract RequestContract is AccountContract{ // Request Contract
+
+
+    struct Request { // Request structure
+        uint reqId;
+        address from;
+        address to;
+        bytes32 status;
+        uint256 date;
+        uint256 amount;
+        uint256 due_amount;
+        uint256 duration;
+        bytes32 purpose;
+         }
+    uint totalReq = 0;
+    // map of Requests
+    mapping(uint => Request) request;
+    mapping(address => uint[]) incomingRequest;
+    mapping(address => uint[]) outgoingRequest;
+   
+
+    //Events for requests
+    event RequestMoney( uint indexed reqId,address indexed from, address indexed to);
+    event AcceptRequest(uint indexed reqId, address indexed from, address indexed to);
+   // event RejectRequest(uint indexed reqid, address indexed from, address indexed to);
+       event Payback(uint indexed reqid, address indexed from, address indexed to);
+
+    function borrowRequest(address userId,uint256 amount, uint256 duration, bytes32 purpose) {
+
+        totalReq++;
+        uint256 rhs=( ((lenderAccounts[userId].interest/100)*100)+(lenderAccounts[userId].interest%100) )/100;
+        uint256 lhs=amount/100;
+       uint256 temp =amount+(lhs*rhs);
+        request[totalReq] = Request({
+            reqId: totalReq,
+            from:msg.sender,
+            to: userId,
+            status: "requested",
+            date: now,
+            amount: amount ,
+            duration: duration,
+            purpose: purpose,
+            due_amount:temp
+          
+        });
+
+        outgoingRequest[msg.sender].push(totalReq);
+        incomingRequest[userId].push(totalReq);
+        RequestMoney(totalReq,userId, msg.sender); // request event
+
+
+    }
+    
+    // accept request function
+    function acceptRequest(uint reqid) payable {
+        if (reqid > totalReq || reqid <= 0 || msg.value != request[reqid].amount || request[reqid].status !='requested' ) {
+            throw;
+        } else {
+            if (request[reqid].to == msg.sender) {
+                request[reqid].status = "accepted";
+                  request[reqid].from.send(msg.value);
+                
+                            }
+        }
+        AcceptRequest(reqid, msg.sender, request[reqid].from); // accept event
+    }
+    
+     // payback request function
+    function payback(uint reqid) payable {
+        if (reqid > totalReq || reqid <= 0 || msg.value != request[reqid].amount || request[reqid].status !='accepted'   ) {
+            throw;
+        } else {
+            if (request[reqid].from == msg.sender) {
+                request[reqid].status = "settled";
+                  request[reqid].to.send(msg.value);
+                
+                }
+        }
+        Payback(reqid, msg.sender, request[reqid].to); // accept event
+    }
+   
+    // reject request function
+    // function rejectRequest(uint reqid) {
+    //     if (reqid > totalReq && reqid <= 0) {
+    //         throw;
+    //     } else {
+    //         if (request[reqid].to == msg.sender) {
+    //             request[reqid].status = "rejected";
+    //                       }
+    //     }
+    //     RejectRequest(reqid, msg.sender, request[reqid].from); // reject event
+    // }
+    
+    // get incoming request function
+    function getIncomingRequests() constant returns(uint[]  memory, address[]  memory,  bytes32[]  memory, uint256[]  memory, uint256[]  memory , bytes32[]  memory,uint256[] memory) {
+       
+       uint[] memory id;
+       address[]  memory from;
+       bytes32[] memory status;
+      
+       uint256[] memory  amount;
+       uint256[] memory duration;
+       bytes32[] memory purpose;
+       uint256[] memory due_amount;
+       
+       id = new uint[](incomingRequest[msg.sender].length);
+       from = new address[](incomingRequest[msg.sender].length);
+       status = new bytes32[](incomingRequest[msg.sender].length);
+       
+       amount = new uint256[](incomingRequest[msg.sender].length);
+       duration = new uint256[](incomingRequest[msg.sender].length);
+       purpose = new bytes32[](incomingRequest[msg.sender].length);
+       due_amount = new uint256[](incomingRequest[msg.sender].length);
+       
+        for(uint i=0; i< incomingRequest[msg.sender].length;i++) {
+            uint  reqId = incomingRequest[msg.sender][i];
+            id[i] = request[reqId].reqId;
+            from[i] = request[reqId].from;
+            status[i] = request[reqId].status;
+           
+            amount[i] = request[reqId].amount;
+            purpose[i] = request[reqId].purpose;
+            duration[i] = request[reqId].duration;
+            due_amount[i] = request[reqId].due_amount;
+            
+         }
+         return (id,from,status,amount,duration,purpose,due_amount);
+    }
+    
+     // get outgoing request function
+    function getOutgoingRequests() constant returns(uint[]  memory, address[]  memory,  bytes32[]  memory, uint256[]  memory, uint256[]  memory , bytes32[]  memory,uint256[] memory) {
+       
+       uint[] memory id;
+       address[]  memory from;
+       bytes32[] memory status;
+       uint256[] memory  amount;
+       uint256[] memory duration;
+       bytes32[] memory purpose;
+       uint256[] memory due_amount;
+       id = new uint[](outgoingRequest[msg.sender].length);
+       from = new address[](outgoingRequest[msg.sender].length);
+       status = new bytes32[](outgoingRequest[msg.sender].length);
+       amount = new uint256[](outgoingRequest[msg.sender].length);
+       duration = new uint256[](outgoingRequest[msg.sender].length);
+       purpose = new bytes32[](outgoingRequest[msg.sender].length);
+       due_amount = new uint256[](outgoingRequest[msg.sender].length);
+        for(uint i=0; i< outgoingRequest[msg.sender].length;i++) {
+            uint  reqId = outgoingRequest[msg.sender][i];
+            id[i] = request[reqId].reqId;
+            from[i] = request[reqId].from;
+            status[i] = request[reqId].status;
+            amount[i] = request[reqId].amount;
+            purpose[i] = request[reqId].purpose;
+            duration[i] = request[reqId].duration;
+            due_amount[i] = request[reqId].due_amount;
+         }
+         return (id,from,status,amount,duration,purpose,due_amount);
+    }
+    
+
+}
+
+
 // Digital locker contract
-contract microLending is  AccountContract, RequestContract {
+contract microLending is   RequestContract {
 
     address owner = msg.sender;
     // reset data function
